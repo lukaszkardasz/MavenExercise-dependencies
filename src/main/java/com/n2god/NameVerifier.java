345a03e87feaa0f0c7e691a8d19d1fe89656068a
n2God <kardasz.lukasz@gmail.com>
2021-04-25 12:34:12 +0200

    Initial commit

---
+++ src/main/java/com/n2god/NameVerifier.java
@@ -0,0 +1,11 @@
+package com.n2god;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class NameVerifier {
+    public static void main(String[] args) {
+        String name = "Maciek2";
+        System.out.println(StringUtils.isAlpha(name));
+    }
+}
+
